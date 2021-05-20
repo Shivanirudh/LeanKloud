2ab636b169bcfc9baa1222ca8244d7dc42a6ec1a
Shivanirudh <shivanirudh2001@gmail.com>
2021-05-20 15:17:22 +0530

    added screencasts

--- ToDoList/db.py
+++
@@ -1,9 +0,0 @@
-import MySQLdb
-
-db = MySQLdb.connect(host="localhost", user="shiva", db="mysql", read_default_file="~/.my.cnf")
-
-db.query("SELECT * FROM Patient_Details")
-
-r = db.store_result()
-
-print(r.fetch_row(maxrows=0))
